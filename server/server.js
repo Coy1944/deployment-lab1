6d2809254988f9c9dbef1eca4be55076e0f61787
Coy1944 <Samuelchincks86@gmail.com>
2022-03-16 15:25:34 -0600

    Update1

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/styles", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.css"));
+});
+
+
+const port = process.env.PORT || 4105;
+
+app.listen(port, () => {
+    console.log(`We vibin on port ${port}`);
+});
